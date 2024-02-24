9e70f4b4867b3b29470879f712c56db32d814ca0
Dacsstack <dacsstack@gmail.com>
2024-02-24 02:13:12 -0800

    +startsWith

--- stringmethods.js
+++ stringmethods.js
@@ -4,20 +4,21 @@ let lastName = "Dacallos";
 let position ="Software Dev";
 
 //True or False
-console.log(firstName.startsWith("a"));
-console.log(lastName.endsWith("o"));
-console.log(position.includes("Program"));
+//console.log(firstName.startsWith("a"));
+//console.log(lastName.endsWith("o"));
+//console.log(position.includes("Program"));
 
 //Uppercase/Lowercase
-console.log(firstName.toUpperCase());
-console.log(firstName.toLowerCase());
+//console.log(firstName.toUpperCase());
+//console.log(firstName.toLowerCase());
 
 //Replace some character
-console.log(firstName.replace("c", "C"));
-console.log(lastName.replaceAll("l", "0"));
+//console.log(firstName.replace("c", "C"));
+//console.log(lastName.replaceAll("l", "0"));
 
-console.log(position.substring(0, 8));
-console.log(position.substring(9, 13));
-console.log(position.substring(0, 8));
+//console.log(position.substring(0, 8));
+//console.log(position.substring(9, 13));
+//console.log(position.substring(0, 8));
 
 //console.log(position.slice(2, 2));
+console.log(position.startsWith("S"));
